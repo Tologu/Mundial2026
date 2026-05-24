d4cb096be9d280de222b1392a6883f532c3b6068
Tologu <tomaslopezgu@gmail.com>
2026-05-24 13:36:34 +0200

    Flechas de ranking pulsantes (sube/baja/igual) en clasificacion

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+window.firebaseConfig = {
+  apiKey: "AIzaSyCjtMTpnX0iG5JfDBG45sOS-f2FKzmMHXE",
+  authDomain: "mundial2026-92533.firebaseapp.com",
+  projectId: "mundial2026-92533",
+  storageBucket: "mundial2026-92533.firebasestorage.app",
+  messagingSenderId: "956283760236",
+  appId: "1:956283760236:web:af4a7b6728677dbafcb249"
+};
